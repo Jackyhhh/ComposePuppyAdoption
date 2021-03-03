c4e312140875c30336edbf7111e74dd93b6cdfb0
JackyZ <897254736@users.noreply.github.com>
2021-03-03 11:59:22 +0800

    bug fix

--- app/src/main/java/com/example/androiddevchallenge/Dog.kt
+++ app/src/main/java/com/example/androiddevchallenge/Dog.kt
@@ -21,7 +21,7 @@ data class Dog(
     val name: String,
     val location: String,
     val age: String,
-    val gender: String,
     val breed: String,
+    val gender: String,
     @DrawableRes val picture: Int,
 )
